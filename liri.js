bc063d704a78c3115d0cf88e714b133f06432a2f
Austin Graves <austingraves106@aol.com>
2019-10-01 23:51:57 -0500

    I have finished all features with the exception of the bonus & the defualt values for both spotify and the movie. I am having some error issues from it that i need to sort out.

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const fs = require('fs');
 const Spotify = require('node-spotify-api');
 const axios = require('axios');
 const moment = require('moment');
@@ -8,17 +9,22 @@ var spotify = new Spotify(keys.spotify);
 
 
 switch (process.argv[2]) {
-  case 'spotify-this-song': 
-    searchSpotify(process.argv[3]);
+  case 'spotify-this-song':
+    var defaultTitle = 'The Sign';
+    if (process.argv[3] === null) {
+      searchSpotify(defaultTitle);
+    } else {
+      searchSpotify(process.argv[3]);
+    }
     break;
   case 'movie-this':
-
+    searchMovies(process.argv[3]);
     break;
   case 'concert-this':
-
+    searchConcerts(process.argv[3]);
     break;
   case 'do-what-it-says':
-
+    doWhatItSays();
     break;
 }
 
@@ -50,20 +56,52 @@ function searchSpotify(userInput) {
   });
 };
 
-function searchConcerts() {
-  axios('https://rest.bandsintown.com/artists/Daughtry/events?app_id=codingbootcamp').then(function(err, data) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(data.data.venue.name);
-      // var data = {
-      //   Venue: data.venue.name
-      // }
-    }
+function searchConcerts(userInput) {
+  axios(`https://rest.bandsintown.com/artists/${userInput}/events?app_id=codingbootcamp`).then(function(data) {
+      var state = data.data[0].venue.region;
+      var city = data.data[0].venue.city;
+      var eventDate = data.data[0].datetime;
+      var eventData = {
+        Venue: data.data[0].venue.name,
+        Location: `${city}, ${state}`,
+        Date: moment(eventDate).format('MM/DD/YYYY')
+      }
+      console.log(eventData);
   });
 }
 
 
+function searchMovies(userInput) {
+  axios(`http://www.omdbapi.com/?apikey=trilogy&t=${userInput}`)
+  .then(function(results, err) {
+    if (err) {
+      console.log(err);
+    } else {
+      // console.log(results.data);
+      var movieData = {
+        Title: results.data.Title,
+        ReleaseYear: results.data.Year,
+        Rating: results.data.Rated,
+        RottenTomatosRating: results.data.Ratings[1].Value,
+        CountryOfProduction: results.data.Country,
+        LanguageOfTheMovie: results.data.Language,
+        PlotOfTheMovie: results.data.Plot,
+        ActorsInTheMovie: results.data.Actors.split(',')
+      }
+      console.log(movieData);
+    }
+  });
+}
 
-
-searchConcerts()
+function doWhatItSays() {
+fs.readFile('random.txt', 'utf8', function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+      var newDataArray = data.split(',');
+      searchSpotify(newDataArray[1]);
+      
+    }
+  })
+}
